cdbdfa5a4a1f6aa1229dc9675d04d19979c7b69f
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-08-07 16:19:01 +0300

    created Home component

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import { from } from "@apollo/client";
+import React from "react";
+
+
+export default function Home(){
+    
+    return(
+        <>
+            <div className="container-fluid">
+                
+
+            </div>
+    
+        </>
+
+    )
+
+};
