0983ca4e42f629aafe24c5447acd040bd2b53984
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-08-07 16:23:50 +0300

    added navbar compotents

---
+++ src/Navbar.js
@@ -0,0 +1,32 @@
+import React from "react"
+
+<nav className="navbar navbar-default">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-2" style="background-color:blueeviolet ;">1</div>
+          <div className="col-sm-3" style="background-color:aqeua;">
+            
+            <div className="navbar-header" style="max-width:fit-content;display: flex;">
+              <a className="navbar-brand" href="#">
+                {/* <img src="static/images/icon-linkedin.png" width="20" height="20" className="d-inline-block  align-top mr-1" alt="logo" loading="lazy"> */}
+                
+              </a> 
+              <form className="form-inline" style="padding: 10px;" >
+                {/* <input className="form-control " type="search" placeholder="Search" aria-label="Search"> */}
+              </form>
+            </div>
+          </div>
+          <div className="col-sm-7" style="background-color:broswn ;">
+            <ul className="nav navbar-nav" style="padding: 0px; height:20px;">
+              <li className="active"><a href="#">Home</a></li>
+              
+              <li><a href="#">Page 1</a></li>
+              <li><a href="#">Page 2</a></li>
+              <li><a href="#">Page 3</a></li>
+            </ul>
+          </div>
+          </div>
+        </div>
+        
+        
+    </nav>
