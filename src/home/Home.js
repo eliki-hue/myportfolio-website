5a587dacec78a2cc27a9ee8635d7899c217eef19
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-09-13 19:20:58 +0300

    added contact component that utilizes Email.js to send emails

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,116 +1,68 @@
 import { from } from "@apollo/client";
 import React from "react";
-import './home.css'
-import Container from 'react-bootstrap/Container';
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import ThemeProvider from 'react-bootstrap/ThemeProvider';
-import Button from 'react-bootstrap/Button';
+import "./home.css";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import ThemeProvider from "react-bootstrap/ThemeProvider";
+import Button from "react-bootstrap/Button";
 // import bus from './static/media/bus.jpg'
-import CardGroup from 'react-bootstrap/CardGroup';
-import myimage from '../images/my-clear-image.png'
-import python from '../images/python-logo.png'
-import JS from '../images/js-logo.png'
-import html from '../images/html-logo.png'
-import carditem from './CardItems'
+import CardGroup from "react-bootstrap/CardGroup";
+import myimage from "../images/my-clear-image.png";
+import python from "../images/python-logo.png";
+import JS from "../images/js-logo.png";
+import html from "../images/html-logo.png";
+import carditem from "./CardItems";
 import CardItems from "./CardItems";
 import Biography from "./Biography";
 import Services from "./services/Services";
 import Portfolio from "./portfolio/Portfolio";
+import ContactUs from "./footer/Contact";
 
-
-export default function Home(){
-    
-    return(
-
-    
-      <>
-      
+export default function Home() {
+  return (
+    <>
       <Container fluid className="parent">
-         <Container className="intro">
+        <Container className="intro">
           <Row className="intro-row">
-            
-         <Col  xs={8} className="intro-text"><div>
-        
-
-        {/* <h1>Hi!</h1> */}
-        <h3 className="text-mut.ed">I am <span className="myname">Elijah Kiragu</span></h3>
-        <h4 className="text-mutned">Full-stack web developer </h4>
-        <ul>
-          
-          <li><h4>React</h4></li>
-          <li><h4>Python</h4></li>
-          
-          
-        
-        </ul>
-          </div> 
-        </Col>
-        <Col   xs={4} className="myimagecol"><div className="itro-image-parent"><br/><br/><br/>
-            <img className="myimage" src={myimage}/></div></Col>
-       
-       
-      </Row>
-      </Container>
-      
+            <Col xs={8} className="intro-text">
+              <div>
+                {/* <h1>Hi!</h1> */}
+                <h3 className="text-mut.ed">
+                  I am <span className="myname">Elijah Kiragu</span>
+                </h3>
+                <h4 className="text-mutned">Full-stack web developer </h4>
+                <ul>
+                  <li>
+                    <h4>React</h4>
+                  </li>
+                  <li>
+                    <h4>Python</h4>
+                  </li>
+                </ul>
+              </div>
+            </Col>
+            <Col xs={4} className="myimagecol">
+              <div className="itro-image-parent">
+                <br />
+                <br />
+                <br />
+                <img className="myimage" src={myimage} />
+              </div>
+            </Col>
+          </Row>
+        </Container>
       </Container>
       <div className="container outer-parent">
-      
-     <Biography/>
-     <Services/>
-     <Portfolio/>
-     
-     
-     
-
-      {/* <Container className="">
-        <Row>
-          <Col >
-        <div className="flip-box">
-          <div className="flip-box-inner">
-            <div className="flip-box-front">
-            
-              <Card style={{margin: '10px',width: '18rem'}}>
-              <Card.Img variant="top" src={myimage} />
-              
-              <Card.Footer>
-                <li className="text-muted">Frontend sAFIRI</li>
-              </Card.Footer>
-            </Card> 
-            </div>
-            <div className="flip-box-back">
-            <Card style={{margin: '10px',width: '18rem'}}>
-              <Card.Img variant="top" src={bus} />
-              
-              <Card.Footer>
-                <li className="text-muted">Frontend sAFIRI</li>
-              </Card.Footer>
-            </Card>
-            
-            </div>
-          </div>
-        </div>
-        </Col>
-        </Row>
-      <br/><br/>
-
-
-      <Row >
-        <CardItems myimage={require('../images/my-clear-image.png')} bus={require('./static/media/bus.jpg')}/>
-        <CardItems myimage={require('../images/my-clear-image.png')} bus={require('./static/media/bus.jpg')}/>
-        <CardItems myimage={require('../images/my-clear-image.png')} bus={require('./static/media/bus.jpg')}/>
-      </Row>
-        </Container>   */}
-        </div>
-      </>
+        <Biography />
+        <Services />
+        <Portfolio />
+        <ContactUs/>
         
 
 
-          
-    
-        
-
-    )
-
-};
+      </div>
+    </>
+  );
+}
---
+++ src/home/footer/Contact.js
@@ -0,0 +1,30 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+const ContactUs = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_iv4g50e', 'template_o0gt4ap', form.current, 'sQQgLC14Rgr5kC5EI')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
+export default ContactUs;
