2543b80a00ecb9199288d571bcda2b2362e3f235
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-10-05 12:49:17 +0300

    did npm run build

--- src/home/SingleProject/SingleProject.js
+++ src/home/SingleProject/SingleProject.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import "./singleProject.css"
+import Navigation from '../../navbar/Navbar';
 
 function SingleProject({img1, img2, img3, category, client, date, url, description}) {
   return (
     <div>
         <div className='container-fluid single-project-header'>
             <nav>
-                <p>navbar</p>
+                <Navigation/>
             </nav>
-            <div className='text-center'>
-                <h1>PORTFOLIAL DETAILS</h1>
+            <div className='text-center '>
+                <h1>PROJECT DETAILS</h1>
             </div>
 
         </div>
@@ -17,27 +18,27 @@ function SingleProject({img1, img2, img3, category, client, date, url, descripti
             <div className='col-8 carousel-images '>
                 <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
                     <ol className="carousel-indicators">
-                        <li data-target="#carouselExampleIndicators" data-slide-to={img1} className="active"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to={img2}></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                         <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
                     </ol>
-                    <div className="carousel-inner mt-4 bg-dark ">
+                    <div className="carousel-inner   ">
                         <div className="carousel-item active">
-                        <img className="d-block w-30" src={img1} alt="First slide"/>
+                        <img className="d-block w-" src={img1} alt="First slide"/>
                         </div>
                         <div className="carousel-item">
-                        <img className="d-block w-30" src={img2} alt="Second slide"/>
+                        <img className="d-block w-" src={img2} alt="Second slide"/>
                         </div>
                         <div className="carousel-item">
-                        <img className="d-block w-30" src={img3} alt="Third slide"/>
+                        <img className="d-block w-" src={img3} alt="Third slide"/>
                         </div>
                     </div>
                     <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="carousel-control-prev-icon control" aria-hidden="true"></span>
                         <span className="sr-only">Previous</span>
                     </a>
                     <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="carousel-control-next-icon control" aria-hidden="true"></span>
                         <span className="sr-only">Next</span>
                     </a>
                 </div>
@@ -58,7 +59,10 @@ function SingleProject({img1, img2, img3, category, client, date, url, descripti
             </div>
 
         </div>
-
+        <div className='container-fluid text-center text-white single-projectfooter'>
+            <h5>All Rights Reserved</h5>
+            <h5>Designed by Elijah Kiragu</h5>
+        </div>
     </div>
   )
 }
